f59e4000f64a52fd086075b80c8fd5a0617ef2e0
Minhaj <minhaj.sixbyte@gmail.com>
2024-02-21 15:37:40 -0600

    link with glf3, first window

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,36 @@
 #include <adder.h> // after installing library to /usr/local/lib
                    // and header to /usr/local/include
 
+#include <GLFW/glfw3.h>
+
 int main() {
     printf("Hello world\n");
     std::cout << simplemath::add(5, 30) << std::endl;
-    return 0;
+
+    GLFWwindow* window;
+
+    if( !glfwInit() ) {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "GLFW Window", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
