72cdce6191fe1b3a3a4fa0bf14ab7b18e5cbe27d
Minhaj <minhaj.sixbyte@gmail.com>
2024-02-21 13:34:48 -0600

    install lib to /usr/share/lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <iostream>
-#include "library/src/adder.h"
+// #include "library/src/adder.h"
+#include <adder.h> // after installing library to /usr/local/lib
+                   // and header to /usr/local/include
 
 int main() {
     printf("Hello world\n");
-    std::cout << simplemath::add(5, 19) << std::endl;
+    std::cout << simplemath::add(5, 30) << std::endl;
     return 0;
 }
